af8c59df4d39dfa9cd65a1e93e039f5f3c136b36
Gabi Dobocan <gabi@telepat.io>
2022-11-28 23:54:38 +0200

    feat: support all js package managers

--- src/vulnerabilities.js
+++ src/vulnerabilities.js
@@ -43,7 +43,7 @@ const fromNpm = (appPath) =>
     });
   });
 
-const fromYarn = (appPath) =>
+const fromYarnClassic = (appPath) =>
   new Promise((resolve, reject) => {
     exec('yarn audit --json', {cwd: appPath}, (err, stdout, stderr) => {
       if (stderr) {
@@ -88,15 +88,79 @@ const fromYarn = (appPath) =>
     });
   });
 
+const fromYarnOrPnpm = (appPath, usePnpm = false) =>
+  new Promise((resolve, reject) => {
+    exec(
+      usePnpm ? 'pnpm audit --json' : 'yarn npm audit --all --recursive --json',
+      {cwd: appPath},
+      (err, stdout, stderr) => {
+        if (stderr) {
+          reject(new Error(stderr));
+        } else {
+          try {
+            const report = {};
+            const {advisories} = JSON.parse(stdout);
+            Object.values(advisories).forEach((advisory) => {
+              const {findings} = advisory;
+              findings.forEach(({paths, version}) => {
+                paths.forEach((path) => {
+                  const components = path.split('>');
+                  components.forEach((name, index) => {
+                    if (name === '.') {
+                      return;
+                    }
+
+                    const via = components[index + 1];
+
+                    if (!report[name]) {
+                      report[name] = [];
+                    }
+
+                    if (!report[name].find(({source}) => source === advisory.id)) {
+                      report[name].push({
+                        source: advisory.id,
+                        name: advisory.module_name,
+                        version,
+                        title: advisory.title,
+                        url: advisory.url,
+                        severity: advisory.severity,
+                        range: advisory.vulnerable_versions,
+                        via,
+                      });
+                    }
+                  });
+                });
+              });
+            });
+
+            resolve(report);
+          } catch (error) {
+            reject(error);
+          }
+        }
+      },
+    );
+  });
+
+const fromYarn = fromYarnOrPnpm;
+
+const fromPnpm = (appPath) => fromYarnOrPnpm(appPath, true);
+
 const getVulnerabilities = async ({appPath, packageManager = 'npm', onProgress = () => {}}) => {
   let vulnerabilities;
 
-  if (packageManager === 'yarn') {
-    onProgress('Getting vulnerability report from yarn');
-    vulnerabilities = await fromYarn(appPath);
-  } else {
+  if (packageManager === 'npm') {
     onProgress('Getting vulnerability report from npm');
     vulnerabilities = await fromNpm(appPath);
+  } else if (packageManager === 'yarn-classic') {
+    onProgress('Getting vulnerability report from yarn');
+    vulnerabilities = await fromYarnClassic(appPath);
+  } else if (packageManager === 'yarn') {
+    onProgress('Getting vulnerability report from yarn');
+    vulnerabilities = await fromYarn(appPath);
+  } else if (packageManager === 'pnpm') {
+    onProgress('Getting vulnerability report from pnpm');
+    vulnerabilities = await fromPnpm(appPath);
   }
 
   return vulnerabilities;
