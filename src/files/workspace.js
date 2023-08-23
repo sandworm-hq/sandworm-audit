d22b9464fe27f26f9c2406d51a671b9eaceb7a16
Gabi Dobocan <gabi@telepat.io>
2023-08-24 00:26:17 +0300

    feat: support pnpm workspaces

--- src/files/workspace.js
+++ src/files/workspace.js
@@ -1,39 +1,55 @@
 const fs = require('fs');
 const path = require('path');
 const fg = require('fast-glob');
+const yaml = require('js-yaml');
 
 const {loadManifest, getPackageSize} = require('.');
 
 const loadWorkspace = async (startPath) => {
   const resolvedAppPath = path.resolve(startPath);
   const manifestPath = path.join(resolvedAppPath, 'package.json');
-  if (fs.existsSync(manifestPath)) {
+  const pnpmConfigPath = path.join(resolvedAppPath, 'pnpm-workspace.yaml');
+  let packagePaths;
+
+  if (fs.existsSync(pnpmConfigPath)) {
+    const pnpmConfig = yaml.load(fs.readFileSync(pnpmConfigPath, 'utf8'));
+
+    if (Array.isArray(pnpmConfig.packages)) {
+      packagePaths = pnpmConfig.packages;
+    }
+  }
+
+  if (!packagePaths && fs.existsSync(manifestPath)) {
     const manifest = await loadManifest(resolvedAppPath);
 
     if (Array.isArray(manifest.workspaces)) {
-      const entries = await fg(manifest.workspaces, {
-        onlyDirectories: true,
-        unique: true,
-        cwd: resolvedAppPath,
-      });
-
-      const workspaceProjects = await entries.reduce(async (aggPromise, relativePath) => {
-        const agg = await aggPromise;
-        const projectPath = path.join(resolvedAppPath, relativePath);
-        const projectManifest = await loadManifest(projectPath);
-        agg.push({
-          ...projectManifest,
-          relativePath,
-          size: await getPackageSize(projectPath),
-        });
-        return agg;
-      }, Promise.resolve([]));
-
-      return {
-        path: resolvedAppPath,
-        workspaceProjects,
-      };
+      packagePaths = manifest.workspaces;
     }
+  }
+
+  if (packagePaths) {
+    const entries = await fg(packagePaths, {
+      onlyDirectories: true,
+      unique: true,
+      cwd: resolvedAppPath,
+    });
+
+    const workspaceProjects = await entries.reduce(async (aggPromise, relativePath) => {
+      const agg = await aggPromise;
+      const projectPath = path.join(resolvedAppPath, relativePath);
+      const projectManifest = await loadManifest(projectPath);
+      agg.push({
+        ...projectManifest,
+        relativePath,
+        size: await getPackageSize(projectPath),
+      });
+      return agg;
+    }, Promise.resolve([]));
+
+    return {
+      path: resolvedAppPath,
+      workspaceProjects,
+    };
   }
 
   if (resolvedAppPath !== '/') {
